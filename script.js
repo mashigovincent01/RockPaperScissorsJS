36d195b20280e419f86737ebe3f4b55221dd14cb
mashigovincent01 <mashigovincent407@gmail.com>
2023-12-28 14:04:42 +0200

    Final edit

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
-
+let isTimerRunning = false;
 function getComputerChoice(){
     let computerChoice = Math.floor(Math.random()*3);
-    const choices = ["Rock", "Paper", "Scissors"];
+    const choices = ["rock", "paper", "scissors"];
     return choices[computerChoice];
 }
 
@@ -49,31 +49,108 @@ var userScore = 0;
 var computerScore = 0;
 
 document.querySelector("#rock").addEventListener("click", ()=>{
-   var result = singleRound("rock", getComputerChoice());
-    document.querySelector("#result").innerHTML = result;
+   document.getElementById("user-choice").innerHTML = "";
+   var compChoice = getComputerChoice();
+   var result = singleRound("rock", compChoice);
+   timer(0, result, compChoice);
 });
 
 document.querySelector("#paper").addEventListener("click", ()=>{
-    var result = singleRound("paper", getComputerChoice());
-    document.querySelector("#result").innerHTML = result;
+    document.getElementById("user-choice").innerHTML = "";
+    var compChoice = getComputerChoice();
+    var result = singleRound("paper", compChoice);
+    timer(1, result, compChoice);
+    
 });
 
 document.querySelector("#scissors").addEventListener("click", ()=>{
-    var result = singleRound("scissors", getComputerChoice());
-    document.querySelector("#result").innerHTML = result;
+    document.getElementById("user-choice").innerHTML = "";
+    var compChoice = getComputerChoice();
+    var result = singleRound("scissors", compChoice);
+    timer(2, result, compChoice);
 });
 
-function updateScore(res){
-    if(res === 'Tie'){
-        return;
-    }
-    if(res[4] === 'L'){
-        computerScore++;
-    }
-    else{
-        userScore++;
+function finalUpdate(choice, res, compChoice){
+    updateUserChoice(choice);
+    updateComputerChoice(compChoice);
+    updateScore(res);
+    // alert(`User Score: ${userScore} + \n Computer Score: ${computerScore}`)
+    document.querySelector("#result").innerHTML = res;
+    isGameOver();
+    
+}
+
+var index = 0;
+function computerOutput(){
+    let element = document.getElementById("computer-choice");
+    element.innerHTML = options[index];
+    index = (index+1)% options.length;
+}
+
+const options = ["✊", "✋", "✌️"];
+
+
+function timer(choice, res, compChoice){
+
+    if(userScore !== 5 && computerScore !==5){
+        const intervalId = setInterval(computerOutput, 100);
+        setTimeout(()=>{
+            clearInterval(intervalId);
+            finalUpdate(choice, res, compChoice);
+        }, 1000);
     }
 }
 
 
+function updateScore(res){
+    if(computerScore !== 5 && userScore !== 5){
+        if(res === 'Tie'){
+            return;
+        }
+        if(res[4] === 'L'){
+            computerScore++;
+        }
+        else{
+            userScore++;
+        }
+        document.querySelector("#userScore").innerHTML = userScore;
+        document.querySelector("#computerScore").innerHTML = computerScore;
+    }
+}
 
+function isGameOver(){
+    if(userScore === 5){
+        
+        res = "Congradulations you won!";
+    }
+    else if(computerScore === 5){
+
+        res = "Sorry you lost!";
+    }
+    else{
+        return
+    }
+    alert(res);
+    // document.querySelector("h1").innerHTML = res;
+    document.querySelector("#result").innerHTML = res;
+    return
+}
+
+
+function updateUserChoice(i){
+    document.getElementById("user-choice").innerHTML = options[i];
+}
+
+function updateComputerChoice(choice){
+    let i = 0;
+    
+    if(choice === "paper"){
+        i = 1;
+    }
+    else if(choice === "scissors"){
+        i = 2;
+    }
+
+    document.getElementById("computer-choice").innerHTML = options[i];
+    
+}
